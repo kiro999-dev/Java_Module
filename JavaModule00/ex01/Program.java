842f5e3d01e13fbba507dea2ce24cebff58edcc3
Zakaria Khourbach <zkhourba@e1r8p1.1337.ma>
2025-12-25 15:41:20 +0100

    add ex01

---
+++ JavaModule00/ex01/Program.java
@@ -0,0 +1,37 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int number = scan.nextInt();
+            if(number <= 1)
+            {
+                System.err.println(("IllegalArgument"));
+                System.exit(-1);
+            }
+            int count = 0;
+            boolean fact = true;
+            int i = 2;
+            while (i < Math.sqrt(number)) 
+            {
+                count++;
+                if (number % i == 0) {
+                    count++;
+                    fact = false;
+                    break;
+                } 
+                i++;
+            }
+            System.out.print(fact);
+            System.out.print(" ");
+            System.out.println((count));
+            scan.close();
+        } catch (Exception e) {
+            System.err.println(("its BIG number"));
+        }
+      
+    }
+}
